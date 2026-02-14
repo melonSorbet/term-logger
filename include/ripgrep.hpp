8c7d9d151538299adea266ebaed9de8f88955c85
melonSorbet <kilicyunusemre678@gmail.com>
2026-02-14 02:28:34 +0100

    feat: add json to struct function for ripgrep

--- include/ripgrep.hpp
+++ include/ripgrep.hpp
@@ -6,3 +6,34 @@
 std::vector<std::string> get_all_files(std::filesystem::path path);
 
 std::vector<boost::json::value> run_ripgrep(std::string);
+
+struct RipGrepMatch {
+  std::string path;
+  std::string line_text;
+  int line_number;
+  int start;
+  int end;
+  std::string match_text;
+  std::string to_string() const {
+    return "RipGrepMatch { "
+           "path: \"" +
+           path +
+           "\", "
+           "line_text: \"" +
+           line_text +
+           "\", "
+           "line_number: " +
+           std::to_string(line_number) +
+           ", "
+           "start: " +
+           std::to_string(start) +
+           ", "
+           "end: " +
+           std::to_string(end) +
+           ", "
+           "match_text: \"" +
+           match_text + "\" }";
+  }
+};
+
+std::vector<RipGrepMatch> jsonToStruct(std::vector<boost::json::value> matches);
--- src/ripgrep.cpp
+++ src/ripgrep.cpp
@@ -1,11 +1,15 @@
+#include <boost/json.hpp>
 #include <boost/json/parse.hpp>
+#include <cstdio>
 #include <fcntl.h>
 #include <filesystem>
 #include <format>
+#include <print>
 #include <ripgrep.hpp>
 #include <string>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -22,15 +26,46 @@ vector<string> get_all_files(filesystem::path path) {
   }
   return files;
 }
-class RipGrepMatch {
-  std::string path;
-  int line;
-  int start_line;
-  int end_line;
 
-  std::vector<RipGrepMatch> jsonToStruct() {}
+std::vector<RipGrepMatch>
+jsonToStruct(std::vector<boost::json::value> matches) {
+  //    "type" : "match", "data" : {
+  //      "path" : {"text" : "src/tui.cpp"},
+  //               "lines"
+  //          : {"text" : "  std::vector<std::string> entries = {\"cool\"};\n"},
+  //            "line_number" : 19,
+  //            "absolute_offset" : 428,
+  //            "submatches"
+  //          : [ {"match" : {"text" : "coo"}, "start" : 39, "end" : 42} ]
+  std::vector<RipGrepMatch> result;
+  for (auto json : matches) {
+    if (!json.is_object() || json.is_null()) {
+      continue;
+    }
+    auto object = json.as_object();
+    if (object.contains("type") && object.at("type").as_string() == "match") {
+      const auto &data = object.at("data").as_object();
+      std::string path =
+          data.at("path").as_object().at("text").as_string().c_str();
+      std::string line_text =
+          data.at("lines").as_object().at("text").as_string().c_str();
+      int line_number = data.at("line_number").as_int64();
+
+      for (auto &submatch : data.at("submatches").as_array()) {
+        RipGrepMatch rgm;
+        rgm.match_text =
+            submatch.at("match").as_object().at("text").as_string().c_str();
+        rgm.start = submatch.at("start").as_int64();
+        rgm.end = submatch.at("end").as_int64();
+        rgm.path = path;
+        rgm.line_text = line_text;
+
+        result.push_back(std::move(rgm));
+      }
+    }
+  }
+  return result;
 };
-
 std::vector<boost::json::value> run_ripgrep(std::string pattern) {
   if (pattern.empty()) {
     return {};
