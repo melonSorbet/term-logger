9be157c03b376afc10e817cedb83d3ea7b1f1831
melonSorbet <kilicyunusemre678@gmail.com>
2026-02-19 17:30:50 +0100

    fix: add correct movement

--- src/tui.cpp
+++ src/tui.cpp
@@ -1,6 +1,8 @@
+#include <algorithm>
 #include <fstream>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/component_options.hpp>
+#include <ftxui/component/event.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/dom/node.hpp>
@@ -10,23 +12,40 @@
 #include <ripgrep.hpp>
 #include <string>
 #include <vector>
+void load_file(std::vector<std::string> &lines,
+               std::vector<RipGrepMatch> current_matches, int selected) {
+  lines = {};
+
+  std::fstream file(current_matches[selected].path);
+  if (!file) {
+    return;
+  }
+  std::string string = "";
+  while (std::getline(file, string)) {
+    lines.push_back(string);
+  }
+}
 
 using namespace ftxui;
-
 int setupTUI() {
 
   auto screen = ScreenInteractive::Fullscreen();
 
   std::vector<std::string> entries = {};
   std::string pattern_input;
-  InputOption input_option = InputOption();
   std::string hint = "";
   std::vector<RipGrepMatch> current_matches = {};
+  int scroll_position = 0;
+
+  std::vector<std::string> lines = {};
+
+  MenuOption option;
+  InputOption input_option = InputOption();
+  int selected = 0;
 
   input_option.on_change = [&entries, &screen, &pattern_input,
                             &current_matches] {
     screen.Post([&] {
-      // get entries and push them all in preferably in a stack
       auto results = jsonToStruct(run_ripgrep(pattern_input));
       entries = {};
       for (auto result : results) {
@@ -56,41 +75,80 @@ int setupTUI() {
 
   Component input_path = Input(&pattern_input, &hint, input_option);
 
-  int selected = 0;
+  option.on_change = [&lines, &current_matches, &selected] {};
 
-  MenuOption option;
-
-  std::vector<std::string> lines = {};
-  option.on_enter = [&lines, &current_matches, &selected] {
-    lines = {};
-
-    std::fstream file(current_matches[selected].path);
-    if (!file) {
-      return;
-    }
-    std::string string = "";
-    while (std::getline(file, string)) {
-      lines.push_back(string);
-    }
-  };
+  // add event to listen for up arrow and down error to only controll movement
+  // add event to listen on mouse button click and go into used editor on teh
+  // file add event to listen to mouse wheel and up and down for scrolling
+  // add event for listening to n and N to jump to the next and last marked word
+  // add event for ctrl c and and y to copy
+  // add event for h for help
 
   auto menu = Menu(&entries, &selected, option);
+  float scroll_y = 0.1;
+  SliderOption<float> option_y;
+  option_y.value = &scroll_y;
+  option_y.min = 0.f;
+  option_y.max = 1.f;
+  option_y.increment = 0.1f;
+  option_y.direction = Direction::Down;
+  option_y.color_active = Color::Yellow;
+  option_y.color_inactive = Color::YellowLight;
+  auto scrollbar_y = Slider(option_y);
 
-  auto left = Container::Vertical(
-      {menu | frame | border | flex,
-       input_path | frame |
-           size(ftxui::WIDTH, ftxui::EQUAL, (Terminal::Size().dimx / 2)) |
-           size(ftxui::HEIGHT, EQUAL, 1) | border});
+  // mark matches
 
-  int scroll_position = 0;
+  // Open file with Enter
 
-  auto text_preview = ftxui::Renderer([&] {
+  auto content = ftxui::Renderer([&] {
     std::vector<ftxui::Element> elements;
     for (const auto &line : lines) {
-      elements.push_back(text(line));
+      elements.push_back(ftxui::text(line));
     }
-    return vbox(elements) | vscroll_indicator | frame | flex;
+
+    return ftxui::vbox(elements) | ftxui::focusPositionRelative(0, scroll_y) |
+           ftxui::frame | ftxui::flex;
   });
+
+  auto text_preview = ftxui::Container::Horizontal({
+
+      content,
+      scrollbar_y,
+
+  });
+
+  // auto text_preview_with_input = CatchEvent(text_preview, [&](Event event) {
+  //   // Mouse scroll wheel
+  //   if (event.is_mouse()) {
+  //     if (event.mouse().button == Mouse::WheelDown) {
+  //       scroll_y = std::min(1.f, scroll_y + 0.05f);
+  //       return true;
+  //     }
+  //     if (event.mouse().button == Mouse::WheelUp) {
+  //       scroll_y = std::max(0.f, scroll_y - 0.05f);
+  //       return true;
+  //     }
+  //   }
+
+  //  if (event == Event::ArrowDown) {
+  //    scroll_y = std::min(1.f, scroll_y + 0.05f);
+  //    return true;
+  //  }
+  //  if (event == Event::ArrowUp) {
+  //    scroll_y = std::max(0.f, scroll_y - 0.05f);
+  //    return true;
+  //  }
+  //  if (event == Event::PageDown) {
+  //    scroll_y = std::min(1.f, scroll_y + 0.2f);
+  //    return true;
+  //  }
+  //  if (event == Event::PageUp) {
+  //    scroll_y = std::max(0.f, scroll_y - 0.2f);
+  //    return true;
+  //  }
+
+  //  return false;
+  //});
 
   auto window_1 = Renderer(text_preview, [&] {
     return vbox({
@@ -98,11 +156,56 @@ int setupTUI() {
                separator(),
                text_preview->Render() | flex,
            }) |
+
            border;
   });
 
-  auto window_container = Container::Horizontal({left | flex, window_1 | flex});
+  auto left = Container::Vertical(
+      {input_path | frame |
+           size(ftxui::WIDTH, ftxui::EQUAL, (Terminal::Size().dimx / 2)) |
+           size(ftxui::HEIGHT, EQUAL, 1) | border,
+       menu | frame | border | flex});
 
+  auto left_renderer = Renderer(left, [&] {
+    return vbox({
+        menu->Render() | frame | border | flex,
+        input_path->Render() | frame |
+            size(ftxui::WIDTH, ftxui::EQUAL, (Terminal::Size().dimx / 2)) |
+            size(ftxui::HEIGHT, EQUAL, 1) | border,
+    });
+  });
+  auto window_container =
+      CatchEvent(Container::Horizontal({left_renderer | flex, window_1 | flex}),
+                 [&](Event event) {
+                   if (event == Event::Escape ||
+                       event.is_character() && event.character() == "q") {
+                     screen.ExitLoopClosure();
+                     return true;
+                   }
+
+                   if (event == Event::ArrowUp) {
+                     // move up in the list
+                     selected = std::max(0, selected - 1);
+                     option.on_change();
+                     load_file(lines, current_matches, selected);
+                     return true;
+                   }
+
+                   if (event == Event::ArrowDown) {
+                     // move down in the list
+                     selected = std::min((int)entries.size() - 1, selected + 1);
+                     option.on_change();
+                     screen.PostEvent(Event::Custom);
+                     load_file(lines, current_matches, selected);
+                     return true;
+                   }
+                   if (event == Event::Return) {
+                   }
+
+                   return false;
+                 });
+
+  input_path->TakeFocus();
   screen.Loop(window_container);
   return 1;
 }
